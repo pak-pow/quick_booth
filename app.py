bbee813a3331eeccf56872ae31845f9850ff2d5c
vince <paulag10987@gmail.com>
2026-03-02 20:53:35 +0800

    chore(py): add image route & start test route

--- app.py
+++ app.py
@@ -13,3 +13,15 @@ CORS(app)
 UPLOAD_FOLDER = 'uploads'
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/api/status', methods=['GET']) # type: ignore
+
+def status():
+    return jsonify({"message": "Quick Booth Python Backend is up and running"})
+
+# Image Upload Route
+@app.route('/api/upload', methods=['POST']) #type: ignore
+def upload_image():
+    # 1. Check if the request actually contains a file named 'image'
+    if 'image' not in request.files:
+        return jsonify({"error": "No image sent in request"}), 400
