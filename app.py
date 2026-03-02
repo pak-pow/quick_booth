ada689abf52e5aca95de2813aebc4129d3185499
vince <paulag10987@gmail.com>
2026-03-02 20:44:18 +0800

    chore(py): add setup upload folder path

--- app.py
+++ app.py
@@ -3,3 +3,13 @@ from flask_cors import CORS
 from werkzeug.utils import secure_filename
 import os
 import uuid
+
+app = Flask(__name__)
+
+# enable CORS
+CORS(app)
+
+# Setup Upload Folder
+UPLOAD_FOLDER = 'uploads'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
